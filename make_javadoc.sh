d42fd90099bff586036d9d5e5063922d6739f602
taku0 <mxxouy6x3m_github@tatapa.org>
2013-05-11 20:40:53 +0900

    スタイルシートで字の大きさをデフォルトの大きさにした

--- make_javadoc.sh
+++ make_javadoc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 javadoc -locale ja_JP -sourcepath src/share/classes -breakiterator -encoding UTF-8 -d javadoc -use -charset UTF-8 -docencoding UTF-8 java.util.stream
+sed -i.bak -e 's/font-size:76%;//' javadoc/stylesheet.css
