a571841db594ca75c4eaab69e3977e834972ca00
taku0 <mxxouy6x3m_github@tatapa.org>
2013-06-23 17:06:56 +0900

    ドキュメント全体のタイトルを追加

--- make_javadoc.sh
+++ make_javadoc.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-javadoc -locale ja_JP -noqualifier all -tag apiNote:a:"API注記" -tag implSpec:a:"実装仕様" -sourcepath src/share/classes -breakiterator -encoding UTF-8 -d javadoc -use -charset UTF-8 -docencoding UTF-8 java.util.stream
+javadoc -locale ja_JP -noqualifier all -tag apiNote:a:"API注記" -tag implSpec:a:"実装仕様" -sourcepath src/share/classes -breakiterator -encoding UTF-8 -d javadoc -use -charset UTF-8 -docencoding UTF-8 -windowtitle 'java.util.stream API仕様 非公式翻訳' java.util.stream
 
 cat stylesheet_override.css >> javadoc/stylesheet.css
-
